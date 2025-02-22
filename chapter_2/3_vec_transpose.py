4856abd32d59a01c13418f8dded86103c634aa92
Akanksha <akankshazc23@icloud.com>
2025-02-22 08:42:00 +0530

    Add code block 2.7: demonstrate vector transposition using NumPy

---
+++ chapter_2/3_vec_transpose.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Code block 2.7
+
+# create a vector
+v1 = np.array([[4, 5, 2, 3]])  # row vector
+
+# transpose the vector
+v2 = v1.T  # column vector
+
+# display the vectors
+print("Row Vector: ", v1)
+print("Column Vector: ", v2)

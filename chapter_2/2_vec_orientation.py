842c60d045c4b62b6e2f6b2c3933d732e1d05f5b
Akanksha <akankshazc23@icloud.com>
2025-02-22 08:41:50 +0530

    Add code block 2.5: demonstrate vector orientations using lists and NumPy arrays

---
+++ chapter_2/2_vec_orientation.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Code block 2.5
+
+# list
+v1 = [2, 4, 6, 7]
+
+# array, no orientation
+v2 = np.array([2, 4, 6, 7])
+
+# column vector
+v3 = np.array([[2], [4], [6], [7]])
+
+# row vector
+v4 = np.array([[2, 4, 6, 7]])
+
+# display the vectors
+print("List: ", v1)
+print("Array (no orientation): ", v2)
+print("Column Vector: ", v3)
+print("Row Vector: ", v4)

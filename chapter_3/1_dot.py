c57d8c7582d19c854b1e0def2069bb1209434084
Akanksha <akankshazc23@icloud.com>
2025-02-23 08:00:15 +0530

    Add code block 3.1: demonstrate dot product of two vectors using NumPy

---
+++ chapter_3/1_dot.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Code block 3.1
+
+# define the two vectors
+v1 = np.array([2, 5, 4, 7])
+v2 = np.array([4, 1, 0, 2])
+
+# dot product of the two vectors
+dp = np.dot(v1, v2)
+
+# print the dot product
+print(dp)

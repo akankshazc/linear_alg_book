aca83334524a3a700da9556aee1a9dbde2362921
Akanksha <akankshazc23@icloud.com>
2025-02-23 08:00:58 +0530

    Add code block 3.7: demonstrate Hadamard product of two vectors using NumPy

---
+++ chapter_3/5_hadamard.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Code block 3.7
+
+# define the vectors
+v1 = np.array([2, 5, 4, 7])
+v2 = np.array([1, 3, 2, 6])
+
+# calculate the element wise vector product (Hadamard product)
+v3 = v1 * v2
+
+# print the result
+print(v3)

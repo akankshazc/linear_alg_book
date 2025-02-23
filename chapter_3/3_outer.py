3f2a8fdca003402be4854689a60dd134c4ce38fb
Akanksha <akankshazc23@icloud.com>
2025-02-23 08:00:39 +0530

    Add code block 3.5: demonstrate outer product of two vectors using NumPy

---
+++ chapter_3/3_outer.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Code block: 3.5
+
+# define the vectors
+v1 = np.array([2, 5, 4, 7])
+v2 = np.array([4, 1, 0, 2])
+
+# calculate the outer product
+op = np.outer(v1, v2)
+
+# print the result
+print(op)
